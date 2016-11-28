c93bae91413d6e01803d5365e0c69b1a52e710a2
gunhappy <gunhappy123@gmail.com>
2016-11-29 01:24:50 +0700

    create sample chart with c3

--- hci-datadriven/src/components/Monthly.vue
+++ hci-datadriven/src/components/Monthly.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="monthly">
-    <h1>this is monthly page</h1>
+    <h1>{{msg}}</h1>
+    <div id="chart"></div>
   </div>
 </template>
 
@@ -9,8 +10,21 @@ export default {
   name:'monthly',
   data(){
     return{
-
+      msg:"this is monthly page",
+      chart:""
     }
+  },
+  mounted: function(){
+    console.log("go");
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+          ]
+        }
+    })
   }
 }
 </script>
---
+++ hci-datadriven/webpack.config.js
@@ -0,0 +1,22 @@
+module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        options: vueConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  }
